20e70c5029043a3c91092fae930c849c3a2f3248
Samin-Sadman-Shad <saminsadmanshad@gmail.com>
2025-08-05 12:15:11 +0600

    project file with solution added

---
+++ SprinklerOptimization/Models/Pipe.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SprinklerOptimization.Models
+{
+    internal struct Pipe
+    {
+        public Point Start { get; set; }
+        public Point End { get; set; }
+
+        public Pipe(Point start, Point end)
+        {
+            Start = start;
+            End = end;
+        }
+
+        public Point GetClosestPoint(Point point)
+        {
+            double dx = End.X - Start.X;
+            double dy = End.Y - Start.Y;
+            double dz = End.Z - Start.Z;
+
+            double t = ((point.X - Start.X) * dx + (point.Y - Start.Y) * dy + (point.Z - Start.Z) * dz) /
+                      (dx * dx + dy * dy + dz * dz);
+
+            t = Math.Max(0, Math.Min(1, t));
+
+            return new Point(
+                Start.X + t * dx,
+                Start.Y + t * dy,
+                Start.Z + t * dz
+            );
+        }
+    }
+}
---
+++ SprinklerOptimization/Models/Point.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SprinklerOptimization.Models
+{
+    internal struct Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+
+        public Point(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public double DistanceTo(Point other)
+        {
+            return Math.Sqrt(Math.Pow(X - other.X, 2) + Math.Pow(Y - other.Y, 2) + Math.Pow(Z - other.Z, 2));
+        }
+
+        public override string ToString()
+        {
+            return $"({X:F2}, {Y:F2}, {Z:F2})";
+        }
+    }
+}
